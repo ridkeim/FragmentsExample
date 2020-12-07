90933a629546c81bbbf2d8b22a0af37bee429824
ridkeim <ridkeim@gmail.com>
2020-12-07 18:21:35 +0300

    добавлено динамическое управление фрагментами

--- app/src/main/java/ru/ridkeim/fragmentsexample/DataFragment.kt
+++ app/src/main/java/ru/ridkeim/fragmentsexample/DataFragment.kt
@@ -1,6 +1,7 @@
 package ru.ridkeim.fragmentsexample
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -9,6 +10,11 @@ import android.widget.TextView
 
 class DataFragment : Fragment() {
     private lateinit var textView: TextView
+    private var selectedState = BUTTON_INDEX_DEFAULT
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        Log.d(TAG,"onCreate $this bundle=$savedInstanceState")
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -16,20 +22,43 @@ class DataFragment : Fragment() {
     ): View? {
         val rootView = inflater.inflate(R.layout.fragment_data, container, false)
         textView = rootView.findViewById(R.id.textView)
+        val selectedState = savedInstanceState?.getInt(BUTTON_INDEX, BUTTON_INDEX_DEFAULT)
+            ?: (arguments?.getInt(BUTTON_INDEX) ?: BUTTON_INDEX_DEFAULT)
+        setText(selectedState)
+        Log.d(TAG,"onCreateView selectedState=$selectedState $this")
         return rootView
     }
 
     fun setText(id : Int){
-        if(id != BUTTON_INDEX_DEFAULT){
+        selectedState = id
+        if(selectedState != BUTTON_INDEX_DEFAULT){
             textView.text = resources.getString(R.string.button_pressed_message,id)
         } else{
             textView.text = resources.getString(R.string.text_view)
         }
+        Log.d(TAG,"setText $this")
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        Log.d(TAG,"onDestroyView $this")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.d(TAG,"onDestroy $this")
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        outState.putInt(BUTTON_INDEX,selectedState)
+        Log.d(TAG,"onSaveInstanceState $this bundle=$outState")
     }
 
     companion object {
         const val BUTTON_INDEX = "BUTTON_INDEX"
         const val BUTTON_INDEX_DEFAULT = -1
+        private val TAG = DataFragment::class.qualifiedName
     }
 
 }
--- app/src/main/java/ru/ridkeim/fragmentsexample/MainActivity.kt
+++ app/src/main/java/ru/ridkeim/fragmentsexample/MainActivity.kt
@@ -1,29 +1,55 @@
 package ru.ridkeim.fragmentsexample
 
-import android.annotation.SuppressLint
-import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import androidx.fragment.app.commit
 
 class MainActivity : AppCompatActivity() , ButtonsFragment.OnSelectedButtonListener {
+
+    private var isTwoPane = false
+
+    private var selectedButton = SELECTED_BUTTON_DEFAULT
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        selectedButton = savedInstanceState?.getInt(SELECTED_BUTTON) ?: SELECTED_BUTTON_DEFAULT
+        isTwoPane = findViewById<View>(R.id.fragment2) != null
+        Log.d(TAG,"onCreate isTwoPane=$isTwoPane")
     }
 
-    @SuppressLint("QueryPermissionsNeeded")
     override fun onButtonSelected(index : Int) {
-        val fragment = supportFragmentManager.findFragmentById(R.id.fragment2) as DataFragment?
-        if(fragment == null || !fragment.isVisible){
-            val intent = Intent(this, SecondActivity::class.java).apply {
-                putExtra(DataFragment.BUTTON_INDEX, index)
-            }
-            intent.resolveActivity(packageManager)?.also {
-                startActivity(intent)
-            }
+        val dF = supportFragmentManager.findFragmentByTag(dataFragmentTag) as DataFragment?
+        selectedButton = index
+        if(isTwoPane){
+            val dataFragment =
+                supportFragmentManager.findFragmentById(R.id.fragment2) as DataFragment?
+            dataFragment?.setText(index)
         }else{
-            fragment.setText(index)
+            val dataFragment = DataFragment()
+            dataFragment.arguments = Bundle().apply {
+                putInt(DataFragment.BUTTON_INDEX, index)
+            }
+            supportFragmentManager.commit {
+                replace(R.id.container,dataFragment,dataFragmentTag)
+                addToBackStack(null)
+            }
         }
+        Log.d(TAG,"isTwoPane=$isTwoPane dataFragmentByTag=$dF")
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        outState.putInt(SELECTED_BUTTON,selectedButton)
+    }
+    companion object {
+        private const val dataFragmentTag = "dataFragmentTag"
+        private const val buttonsFragmentTag = "buttonsFragmentTag"
+        private val TAG = MainActivity::class.qualifiedName
+        private const val SELECTED_BUTTON = "selected_button"
+        private const val SELECTED_BUTTON_DEFAULT = -1
+
     }
 }
